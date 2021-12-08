8d54a31e6c96e05ef546b1e98431039311662e99
Amanda Ferreira <amandaf1604@gmail.com>
2021-12-08 12:20:58 -0300

    Novo projeto

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,8 +3,11 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Olá mundo ");
-		System.out.println(" Olá ");
+		
+		System.out.println("Bom dia!");
+		
+		
+		
 	}
 
 }
